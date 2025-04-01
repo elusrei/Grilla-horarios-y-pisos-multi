9d7a7eb5c03e1977d5540343f1f7588c9ee56ea1
lel usb <elianexsequiel@gmail.com>
2025-04-01 14:18:22 -0300

    App terminada pero
    le faltan margenes para tenerlo clean para imprimirlo

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ document.getElementById('exportButton').addEventListener('click', function() {
   // Función para calcular el piso a partir del aula (por si se necesita dinámicamente)
   function getFloor(classroom) {
     const firstDigit = classroom.charAt(0);
-    if (firstDigit === '0') return 'PB';
-    return `P${firstDigit}`;
+if (firstDigit === '0') return 'PB';
+return `P${firstDigit}`;
   }
